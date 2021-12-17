7840cf383bee5ea2c3ae68801f8ca0ecc488fd46
arnold <nganizyo@gmail.com>
2021-12-17 16:30:42 +0000

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,36 @@
 class Ride < ActiveRecord::Base
+    belongs_to :user 
+    belongs_to :attraction
+    validates :user_id, presence: true 
+    validates :attraction_id, presence: true
+
+    def take_ride
+        @user = User.find_by(id: self.user_id)
+        @attraction = Attraction.find_by(id: self.attraction_id)
+        tallenough = false
+        enoughtickets = false
+        comment = ''
+
+        if @user && @attraction
+            enoughtickets = true if @user.tickets >= @attraction.tickets
+            tallenough = true if @user.height >= @attraction.min_height
+        end
+
+        if tallenough && enoughtickets
+            @user.tickets = @user.tickets - @attraction.tickets
+            @user.happiness += @attraction.happiness_rating
+            @user.nausea += @attraction.nausea_rating
+            @user.save
+        elsif !tallenough && !enoughtickets
+            comment = "Sorry. You do not have enough tickets to ride the #{@attraction.name}. You are not tall enough to ride the #{@attraction.name}."
+        elsif !enoughtickets
+            comment = "Sorry. You do not have enough tickets to ride the #{@attraction.name}."
+        elsif !tallenough
+            comment = "Sorry. You are not tall enough to ride the #{@attraction.name}."
+        end
+        
+        comment
+    end
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ActiveRecord::Base
+    has_secure_password
+    has_many :rides
+    has_many :attractions, through: :rides
+    validates :name, presence: true
+    validates :password, presence: true
+    #validates :nausea, presence: true
+    #validates :happiness, presence: true
+    #validates :tickets, presence: true 
+    #validates :height, presence: true
+
+    def mood
+        self.nausea >= self.happiness ? "sad" : "happy"
+    end
 end
+
